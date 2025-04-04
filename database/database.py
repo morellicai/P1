310858b1fcfb5c3d20bae2f53d031a224baec7e6
Caio Morelli <caiomorelli.it@gmail.com>
2025-04-03 22:38:45 -0300

    Iniciando a conexão entre MySQL e Python

--- database/database.py
+++ database/database.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host = 'localhost',
+    database = '',
+    user = 'root',
+    password = 'manager'
+)
+
+if conn.is_connected():
+    db_info = conn.get_server_info()
+    print("Conectado ao servidor MySQL versão", db_info)
+
+    cursor = conn.cursor()
+    cursor.execute("create database CRUD();")
+    linha = cursor.fetchone()
+    print("Conectado ao banco de dados ", linha)
+
+if conn.is_connected():
+    cursor.close()
+    conn.close()
+    print("Conexão ao MySQL foi encerrada")
